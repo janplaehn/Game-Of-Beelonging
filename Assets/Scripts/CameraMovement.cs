840777a2f2b5dafd706b8d5d2648cbd01641536f
janplaehn <janplaehn@yahoo.de>
2018-01-17 15:23:26 +0100

    Boundaries
    Created boundaries for the player movement based on the offset of the camera.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour {
 
     public float speed;
+    public float offset;
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +15,6 @@ public class CameraMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         this.transform.position = new Vector3(this.transform.position.x + speed/100, this.transform.position.y, this.transform.position.z);
-
+        offset = transform.position.x;
     }
 }
--- Assets/Scripts/MainBeeMovement.cs
+++ Assets/Scripts/MainBeeMovement.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class MainBeeMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Vector2 newPosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
-        this.transform.position = newPosition;
+    private GameObject MainCamera;
+    private Vector2 newPosition;
+    private float leftBoundary;
+    private float rightBoundary;
+    private float topBoundary;
+    private float bottomBoundary;
 
+    void Start () {
+        MainCamera = GameObject.Find("Main Camera");
+        leftBoundary = -8.5f;
+        rightBoundary = 8.5f;
+        topBoundary = 4.75f;
+        bottomBoundary = -4.75f;
+    }
+	
+	void Update () {
+        leftBoundary = MainCamera.GetComponent<CameraMovement>().offset - 8.5f;
+        rightBoundary = MainCamera.GetComponent<CameraMovement>().offset + 8.5f;
+        newPosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+
+        if (newPosition.x >= leftBoundary && newPosition.x <= rightBoundary &&  newPosition.y <= topBoundary && newPosition.y >= bottomBoundary)
+        {
+            this.transform.position = newPosition;
+        }
+        else
+        {
+            this.transform.position = new Vector3(this.transform.position.x + MainCamera.GetComponent<CameraMovement>().speed / 100, this.transform.position.y, this.transform.position.z);
+        }
     }
 }
