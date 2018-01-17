07c748cc7152e22a3da4327a0fe39cced73fa997
janplaehn <janplaehn@yahoo.de>
2018-01-17 12:25:27 +0100

    Initial commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(this.transform.position.x + speed/100, this.transform.position.y, this.transform.position.z);
+
+    }
+}
---
+++ Assets/Scripts/MainBeeMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainBeeMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 newPosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        this.transform.position = newPosition;
+
+    }
+}
