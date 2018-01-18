552593aca236b706dabe31ab08c58c7473ed2b28
janplaehn <janplaehn@yahoo.de>
2018-01-18 20:04:46 +0100

    Added Parallaxing Script

---
+++ Assets/Scripts/Parallaxing.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//Reference: https://www.youtube.com/watch?v=QkisHNmcK7Y
+public class Parallaxing : MonoBehaviour {
+    public Transform[] backgrounds;
+    private float[] parallaxScales;
+    public float smoothing = 1f;
+    private Transform cam;
+    private Vector3 previousCamPos;
+
+    void Awake() {
+        cam = Camera.main.transform;
+    }
+
+    void Start() {
+        previousCamPos = cam.position;
+        parallaxScales = new float[backgrounds.Length];
+        for (int i = 0; i < backgrounds.Length; i++) {
+            parallaxScales[i] = backgrounds[i].position.z * -1;
+        }
+    }
+
+    void Update() {
+        for (int i = 0; i < backgrounds.Length; i++) {
+            float parallax = (previousCamPos.x - cam.position.x) * parallaxScales[i];
+            float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+        }
+        previousCamPos = cam.position;
+    }   
+}
