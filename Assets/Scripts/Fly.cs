c1c7eff88834fe3ac02ec8ede2cc3deb31bbd93e
janplaehn <janplaehn@yahoo.de>
2018-01-18 18:50:46 +0100

    multiple changes
    added fly swarms, changed death behaviour of enemies and player and added a health system for enemies

---
+++ Assets/Scripts/Fly.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour {
+
+    enum Direction { Up, Down};
+    Direction moveDirection;
+    private Vector2 startPosition;
+    public float curveExtremes;
+    public float curveSpeed;
+    public int healthPoints;
+    public bool isAlive;
+
+    void Start () {
+        this.GetComponent<Rigidbody2D>().gravityScale = 0.0f;
+        moveDirection = Direction.Up;
+        startPosition = transform.position;
+        isAlive = true;
+        healthPoints = 1;
+    }
+	
+	void Update () {
+		if (transform.position.y <= -6) {
+            Destroy(this.gameObject);
+        }
+        if (isAlive) {
+            Move();
+            if (healthPoints <= 0) {
+                Die();
+            }
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D otherCollider) {
+        if (otherCollider.tag == "PlayerBullet" && isAlive) {
+            Destroy(otherCollider.transform.root.gameObject);
+            healthPoints--; 
+        }
+    }
+
+    void Die() {
+        transform.position = new Vector3(transform.position.x, transform.position.y + 0.2f, transform.position.z);
+        this.GetComponent<Rigidbody2D>().gravityScale = 2.0f;
+        isAlive = false;
+    }
+
+    void Move() {
+        if (moveDirection == Direction.Up) {
+            transform.position = new Vector3(transform.position.x, transform.position.y + curveSpeed, transform.position.z);
+            if (transform.position.y > startPosition.y + curveExtremes) {
+                moveDirection = Direction.Down;
+            }
+        }
+        else if (moveDirection == Direction.Down) {
+            transform.position = new Vector3(transform.position.x, transform.position.y - curveSpeed, transform.position.z);
+            if (transform.position.y < startPosition.y - curveExtremes)
+            {
+                moveDirection = Direction.Up;
+            }
+        }
+    }
+}
--- Assets/Scripts/MainBee.cs
+++ Assets/Scripts/MainBee.cs
@@ -12,6 +12,9 @@ public class MainBee : MonoBehaviour {
     private float bottomBoundary;
     public Transform bullet;
     public float bulletOffset;
+    public float speed;
+    public bool isCursorVisible;
+    private bool isAlive;
 
     void Start () {
         MainCamera = GameObject.Find("Main Camera");
@@ -19,15 +22,28 @@ public class MainBee : MonoBehaviour {
         rightBoundary = 8.5f;
         topBoundary = 4.75f;
         bottomBoundary = -4.75f;
+        Cursor.visible = isCursorVisible;
+        isAlive = true;
     }
 	
 	void Update () {
-        Move();
-        if (Input.GetMouseButtonDown(0)) {
-            Shoot();
+        if (isAlive)
+        {
+            Move();
+            if (Input.GetMouseButtonDown(0))
+            {
+                Shoot();
+            }
+            if (Input.GetMouseButtonDown(1))
+            {
+                CallSwarm();
+            }
         }
-        if (Input.GetMouseButtonDown(1)) {
-            CallSwarm();
+        else
+        {
+            if (transform.position.y <= -6) {
+                Destroy(this.gameObject);
+            }
         }
     }
 
@@ -37,7 +53,7 @@ public class MainBee : MonoBehaviour {
         newPosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
 
         if (newPosition.x >= leftBoundary && newPosition.x <= rightBoundary && newPosition.y <= topBoundary && newPosition.y >= bottomBoundary) {
-            this.transform.position = newPosition;
+            transform.position = Vector3.MoveTowards(transform.position, newPosition, speed);
         }
         else {
             this.transform.position = new Vector3(this.transform.position.x + MainCamera.GetComponent<MainCamera>().speed / 100, this.transform.position.y, this.transform.position.z);
@@ -53,14 +69,14 @@ public class MainBee : MonoBehaviour {
     }
 
     void OnTriggerStay2D(Collider2D otherCollider) {
-        if (otherCollider.tag == "Enemy")
-        {
+        if (otherCollider.tag == "Enemy" && otherCollider.transform.root.gameObject.GetComponent<Fly>().isAlive && this.isAlive) {
             Die();
         }
     }
 
-    void Die()
-    {
-        Destroy(this.gameObject);
+    void Die() {
+        transform.position = new Vector3(transform.position.x, transform.position.y + 0.2f, transform.position.z);
+        this.GetComponent<Rigidbody2D>().gravityScale = 2.0f;
+        isAlive = false;
     }
 }
