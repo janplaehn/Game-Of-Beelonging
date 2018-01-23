23a9b69526266c5d057f589fac6793ab4698ee8e
janplaehn <janplaehn@yahoo.de>
2018-01-23 14:12:09 +0100

    added Main Menu, Game Over Screen and "Finish Line"

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLine : MonoBehaviour {
+
+    public string nextSceneName;
+
+    private GameObject MainCamera;
+
+	void Start () {
+        MainCamera = GameObject.Find("Main Camera");
+    }
+	
+	void Update () {
+        if (transform.position.x < MainCamera.transform.position.x + 8)
+        {
+            MainCamera.GetComponent<MainCamera>().speed = 0;
+        }	
+	}
+
+    void OnTriggerStay2D(Collider2D otherCollider) {
+        if (otherCollider.tag == "MainBee") {
+            SceneManager.LoadScene(nextSceneName, LoadSceneMode.Single);
+        }
+    }
+}
--- Assets/Scripts/Menu/RetryButton.cs
+++ Assets/Scripts/Menu/RetryButton.cs
@@ -4,17 +4,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class RetryButton : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     public void Retry() {
         SceneManager.LoadScene(1, LoadSceneMode.Single);
     }
