5c47b0353922ff4704424e2670e3666d0ad55275
janplaehn <janplaehn@yahoo.de>
2018-01-29 22:59:22 +0100

    Added Position Adjustment for Swarm

---
+++ Assets/Scripts/Bees/BeeSwarmAdjust.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class BeeSwarmAdjust : MonoBehaviour {
+
+    public float adjustSpeed;
+
+	void Start () {
+        
+    }
+	
+	void Update () {
+        if (!(transform.position.x < 0.2 && transform.position.x > 0.2)) {
+            Vector3 newPosition = new Vector3(transform.position.x, transform.position.y - getAIBeeCenter() * Time.deltaTime, transform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, newPosition, adjustSpeed * Time.deltaTime);
+        }
+
+    }
+
+    float getAIBeeCenter() {
+        GameObject[] bees = GameObject.FindGameObjectsWithTag("AIBee");
+        float totalYPos = 0;
+        float beeCount = 0;
+        foreach (GameObject bee in bees) {
+            totalYPos += bee.transform.position.y;
+            beeCount++;
+        }
+        return totalYPos / beeCount;
+    }
+}
---
+++ Assets/Scripts/Other/NewCursor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewCursor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+    }
+}
