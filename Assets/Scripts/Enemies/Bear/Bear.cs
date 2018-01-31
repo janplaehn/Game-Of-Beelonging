d625d89de648203fb077efc3efa2a9e6772c2df8
janplaehn <janplaehn@yahoo.de>
2018-01-31 16:18:59 +0100

    Improved Boss Fight

--- Assets/Scripts/Enemies/Bear/Bear.cs
+++ Assets/Scripts/Enemies/Bear/Bear.cs
@@ -6,33 +6,39 @@ public class Bear : MonoBehaviour {
 
     public float healthPoints;
     public float moveSpeed;
-    public Vector3 leftBoundary;
-    public Vector3 rightBoundary;
+    public float leftBoundary;
+    public float rightBoundary;
+    public float targetHeight1;
+    public float targetHeight2;
+    public float targetHeight3;
 
     public GameObject leftarm;
     public GameObject rightarm;
     public GameObject hitbox;
 
     public enum State {AttackForward, AttackBackward, SpawnEnemies, Defend};
-    [ShowOnly] public State fightState;
+    [HideInInspector] public State fightState;
 
     private int attackNumber;
+    private float currentTargetHeight;
 
 
-	void Start () {
+
+    void Start () {
         fightState = State.Defend;
         StartCoroutine(ChangeState(State.AttackForward));
         attackNumber = 0;
+        
     }
 	
 	void Update () {
         switch (fightState) {
             case State.AttackForward:
                 hitbox.SetActive(false);
-                transform.position = Vector3.MoveTowards(transform.position, leftBoundary, moveSpeed * Time.deltaTime);
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(leftBoundary, currentTargetHeight, 0), moveSpeed * Time.deltaTime);
                 leftarm.GetComponent<BearArm>().MoveArm();
                 rightarm.GetComponent<BearArm>().MoveArm();
-                if (transform.position.x <= leftBoundary.x) {
+                if (transform.position.x <= leftBoundary) {
                     rightarm.GetComponent<BearArm>().ChangeDirection();
                     leftarm.GetComponent<BearArm>().ChangeDirection();
                     fightState = State.AttackBackward;
@@ -40,19 +46,21 @@ public class Bear : MonoBehaviour {
                 break;
             case State.AttackBackward:
                 hitbox.SetActive(false);
-                transform.position = Vector3.MoveTowards(transform.position, rightBoundary, moveSpeed * Time.deltaTime);
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(rightBoundary, 0, 0), moveSpeed * Time.deltaTime);
                 leftarm.GetComponent<BearArm>().MoveArm();
                 rightarm.GetComponent<BearArm>().MoveArm();
-                if (transform.position.x >= rightBoundary.x) {
+                if (transform.position.x >= rightBoundary) {
                     rightarm.GetComponent<BearArm>().ChangeDirection();
                     leftarm.GetComponent<BearArm>().ChangeDirection();
                     attackNumber++;
                     if (attackNumber > 3) {
+                        currentTargetHeight = SetRandomHeight();
                         fightState = State.Defend;
                         attackNumber = 0;
                         StartCoroutine(ChangeState(State.AttackForward));
                     }
                     else {
+                        currentTargetHeight = SetRandomHeight();
                         fightState = State.AttackForward;
                     }
                    
@@ -62,6 +70,8 @@ public class Bear : MonoBehaviour {
                 break;
             case State.Defend:
                hitbox.SetActive(true);
+                rightarm.GetComponent<BearArm>().ResetArm();
+                leftarm.GetComponent<BearArm>().ResetArm();
                 break;
             default:
                 break;
@@ -82,5 +92,19 @@ public class Bear : MonoBehaviour {
     IEnumerator ChangeState(State st) {
         yield return new WaitForSeconds(5);
         fightState = st;     
+    }
+
+    float SetRandomHeight() {
+        int temp = Random.Range(0, 3);
+        switch (temp) {
+            case 0:
+                return targetHeight1;
+            case 1:
+                return targetHeight2;
+            case 2:
+                return targetHeight3;
+            default:
+                return targetHeight1;
+        }
     }
 }
--- Assets/Scripts/Enemies/Bear/BearArm.cs
+++ Assets/Scripts/Enemies/Bear/BearArm.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class BearArm : MonoBehaviour {
 
     public float rotateSpeed;
-    public float rotationDuration;
     public Vector3 rotationDirection;
 
     private GameObject shoulder;
@@ -23,6 +22,12 @@ public class BearArm : MonoBehaviour {
         transform.RotateAround(shoulder.transform.position, rotationDirection, rotateSpeed * Time.deltaTime);
     }
 
+    public void ResetArm() {
+        if (transform.rotation.z > 0) {
+            rotationDirection = new Vector3(0, 0, -1);
+            transform.RotateAround(shoulder.transform.position, rotationDirection, rotateSpeed * 10 * Time.deltaTime);
+        }
+    }
    public void ChangeDirection() {
         rotationDirection = new Vector3(0, 0, rotationDirection.z * -1);
     }
