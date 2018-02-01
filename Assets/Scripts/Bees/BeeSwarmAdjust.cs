25abe18ca019ed2db311c4d5c0c70f0aaefb08a5
janplaehn <janplaehn@yahoo.de>
2018-02-01 10:47:17 +0100

    Impproved Bee Swarm Y Position Adjustment

--- Assets/Scripts/Bees/BeeSwarmAdjust.cs
+++ Assets/Scripts/Bees/BeeSwarmAdjust.cs
@@ -9,16 +9,26 @@ public class BeeSwarmAdjust : MonoBehaviour {
     }
 	
 	void Update () {
+        if (Input.GetMouseButton(1)) {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, 0, 0), adjustSpeed * Time.deltaTime);
+        }
+        else {
+            Adjust();
+        }
+       
+
+    }
+
+    void Adjust() {
         if (!(transform.position.x < 0.05 && transform.position.x > 0.05)) {
             Vector3 newPosition = new Vector3(transform.position.x, transform.position.y - getAIBeeCenter() * Time.deltaTime, transform.position.z);
             transform.position = Vector3.MoveTowards(transform.position, newPosition, adjustSpeed * Time.deltaTime);
         }
-
     }
 
     float getAIBeeCenter() {
         GameObject[] bees = GameObject.FindGameObjectsWithTag("AIBee");
-        if (bees.Length == 0) {
+        if (bees.Length <= 1) {
             return 0;
         }
         float totalYPos = 0;
